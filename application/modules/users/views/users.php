7ddb4a87d07d5e439fa64f84670fac8b6d193056
20alim20 <aldrich.lim@conciergeplus.com>
2020-11-08 01:54:45 +0800

    new

--- application/modules/users/views/users.php
+++ application/modules/users/views/users.php
@@ -1,20 +1,28 @@
 <div class="container-fluid">
-    <table class="table table-border table-striped mt-5">
-        <thead>
-            <tr>
-                <th>Full Name</th>
-                <th>Date</th>
-                <th>Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php for ($x = 0; $x < COUNT($userlist); $x++) : ?>
+    <div class="mt-5">
+        <table id="dttableUsers" class="table table-border table-striped">
+            <thead>
                 <tr>
-                    <td><?= $userlist[$x]->fullname ?></td>
-                    <td><?= $userlist[$x]->last_logged_in ?></td>
-                    <td><?= ($userlist[$x]->is_logged_in == "Y" ? "logged in" : "not logged in") ?></td>
+                    <th>Full Name</th>
+                    <th>Date</th>
+                    <th>Status</th>
                 </tr>
-            <?php endfor; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php for ($x = 0; $x < COUNT($userlist); $x++) : ?>
+                    <tr>
+                        <td><?= $userlist[$x]->fullname ?></td>
+                        <td><?= $userlist[$x]->last_logged_in ?></td>
+                        <td><?= ($userlist[$x]->is_logged_in == "Y" ? "logged in" : "not logged in") ?></td>
+                    </tr>
+                <?php endfor; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+        $('#dttableUsers').DataTable();
+    })
+</script>
